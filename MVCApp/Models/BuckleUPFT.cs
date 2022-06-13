89b6d96e9d5b9e6161294dd5fe6b566f320f4a6e
Rajesh kumar <rajesh10063@gmail.com>
2022-06-13 10:16:51 +0530

    buckleup

---
+++ MVCApp/Models/BuckleUPFT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models
+{
+    public class BuckleUPFT
+    {
+        public string Plant { get; set; }
+        public string Family { get; set; }
+        public string ItemCode { get; set; }
+        public string TransmissionSrno { get; set; }
+        public string RearAxleSrno { get; set; }
+
+    }
+}
