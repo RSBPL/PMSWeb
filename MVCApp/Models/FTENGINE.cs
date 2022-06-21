577c69c75db24414204e65d18bdbf3ca12f6e6fb
Rajesh kumar <rajesh10063@gmail.com>
2022-06-21 17:40:43 +0530

    ftengine

--- MVCApp/Models/FTENGINE.cs
+++ MVCApp/Models/FTENGINE.cs
@@ -5,20 +5,16 @@ using System.Web;
 
 namespace MVCApp.Models
 {
-    public class PTBUCKlUP
+    public class FTENGINE
     {
         public string Plant { get; set; }
         public string Family { get; set; }
-        public string ItemCode { get; set; }
         public string JobId { get; set; }
         public string EngineSrno { get; set; }
-        public string BackendSrno { get; set; }
         public string Password { get; set; }
-        public string TractorSrno { get; set; }
-        public string lblBackend { get; set; }
-        public string lblEngine { get; set; }
         public bool isbypass { get; set; }
+        public string Job { get; set; }
         public string lbltractorno { get; set; }
-
+        public string TractorSrno { get; set; }
     }
 }
